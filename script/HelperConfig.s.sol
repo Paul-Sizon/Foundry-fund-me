6ccd6d6dd6c12add1af30efe39f49f25489adc26
paul.sizon <paul.sizon@outlook.com>
2024-10-14 20:01:14 -0300

    fund me contract

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+
+
+contract HelperConfig is Script {
+    // if on local chain (anvil), deploy mock
+    // otherwise, grab existing contract address from live network
+}
+
+
+
+// 1. deploy mock on local anvil chain
+// 2. keep track of the contract address accross the different chains
+
+// Sepolia ETH / USD Address
+// Mainnet ETH / USD Address 
